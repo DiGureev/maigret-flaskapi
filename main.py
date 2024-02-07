e6f6b9316ae24d8ff2788d1cdb1e41d692fb9352
DiGureev <dianagureev@gmail.com>
2024-02-07 13:35:51 +0200

    read from json

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import json
+import os
 from script import main
 import asyncio
 
@@ -7,7 +9,17 @@ app = Flask(__name__)
 @app.route("/<username>/<top>")
 def home(username, top):
     top = int(top)
-    result = asyncio.run(main(username, top))
+    
+    if os.path.exists(f"./reports/{username}{top}.json"):
+        print("Exist")
+        with open(f"./reports/{username}{top}.json","r") as f:
+            result = json.load(f)
+    else:
+        print("Does not Exist")
+        result = asyncio.run(main(username, top))
+        with open(f"./reports/{username}{top}.json", "w") as f:
+            json.dump(result, f)
+
     return result, 200
 
 if __name__ == "__main__":
