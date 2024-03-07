c86f89dfcf29087b2e069de9935d626588fb25ac
DiGureev <dianagureev@gmail.com>
2024-03-07 11:10:15 +0200

    save results in one object

--- main.py
+++ main.py
@@ -6,20 +6,43 @@ import asyncio
 
 app = Flask(__name__)
 
+def readFile(username):
+    result = {}
+    with open(f"./{username}.json","r") as f:
+        result = json.load(f)
+    return result
+
+def writeFile(username, object):
+    with open(f"./{username}.json", "w") as f:
+        json.dump(object, f)
+    return
+
+
 @app.route("/<username>/<top>")
 def home(username, top):
     top = int(top)
-    
-    if os.path.exists(f"./{username}{top}.json"):
-        print("Exist")
-        with open(f"./{username}{top}.json","r") as f:
-            result = json.load(f)
-    else:
-        print("Does not Exist")
-        result = asyncio.run(main(username, top))
-        with open(f"./{username}{top}.json", "w") as f:
-            json.dump(result, f)
 
+    result = {}
+    
+    if top == 100 and os.path.exists(f"./{username}.json"):
+        print("Exist")
+        result = readFile(username)
+    elif top == 100 and os.path.exists(f"./{username}.json") == False:
+        print("Does not Exist")
+
+        result = asyncio.run(main(username, top))
+
+        writeFile(username, result)
+    else:
+        print("Top is not 100")
+        exist_object = readFile(username)
+        result = asyncio.run(main(username, top))
+
+        for key, value in result.items():
+            exist_object[key] = value
+        
+        writeFile(username, exist_object)
+        
     for key in result:
         result[key] = {
             "url_user": result[key]["url_user"],
